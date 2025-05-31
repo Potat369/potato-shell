1e614c723a65729a0138f4b6f20b8316d218c55c
Potat369 <yevheniidemian@gmail.com>
2025-05-31 10:32:37 +0300

    Change scss to css

--- app/init.lua
+++ app/init.lua
@@ -1,11 +1,8 @@
 local App = require("astal.gtk3.app")
 local astal = require("astal")
-local scss = "./style.scss"
-local css = "/tmp/style.css"
+local css = "./style.css"
 
 local InfoStack = require("widgets/info_stack")
-
-astal.exec(string.format("sass %s %s", scss, css))
 
 App:start({
 	css = css,
