2745c0fa780624a061a4d595cf8f443106c41e87
Potat369 <yevheniidemian@gmail.com>
2025-06-02 14:50:14 +0300

    Add debug statememt, because that shit works locally but not from github

--- init.lua
+++ init.lua
@@ -6,6 +6,7 @@ local InfoStack = require("widgets/info_stack")
 local function src(path)
 	local str = debug.getinfo(2, "S").source:sub(2)
 	local s = str:match("(.*/)") or str:match("(.*\\)") or "./"
+	astal.write_file("/home/potat369/debug.txt", s .. path)
 	return s .. path
 end
 
