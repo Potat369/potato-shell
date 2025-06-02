b46cf35733a1596f0be674301308d2b451815a3c
Potat369 <yevheniidemian@gmail.com>
2025-06-02 14:39:15 +0300

    Make style.css relative to source

--- init.lua
+++ init.lua
@@ -1,11 +1,16 @@
 local App = require("astal.gtk3.app")
 local astal = require("astal")
-local css = "./style.css"
 
 local InfoStack = require("widgets/info_stack")
 
+local function src(path)
+	local str = debug.getinfo(2, "S").source:sub(2)
+	local s = str:match("(.*/)") or str:match("(.*\\)") or "./"
+	return s .. path
+end
+
 App:start({
-	css = css,
+	css = src("style.css"),
 	main = function()
 		InfoStack()
 	end,
